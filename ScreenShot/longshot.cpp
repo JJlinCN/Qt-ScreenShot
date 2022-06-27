11df8835f4daa506ee715e7fe1559e40d70e529f
longxinping <1056999736@qq.com>
2022-06-27 11:35:02 +0800

    lxp 长截图

--- ScreenShot/longshot.cpp
+++ ScreenShot/longshot.cpp
@@ -2,44 +2,58 @@
 #include<QPixmap>
 #include<QVBoxLayout>
 #include<QTextEdit>
+#include<QFileDialog>
+#include<QTextDocument>
 #include<QScreen>
 #include<QLabel>
 
+//设置滑动栏
 LongShot::LongShot(QWidget *parent):QWidget(parent),ui(new Ui::Form)
 {
     ui->setupUi(this);
-    ui->scroll->setGeometry(10,10,300,300);
+    ui->scroll->setGeometry(10,10,200,200);
     ui->scroll->setWidgetResizable(false);
     ui->scroll->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
     ui->scroll->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
     ui->scroll->setWidget(ui->scrollAreaWidgetContents);
-    ui->scrollAreaWidgetContents->setGeometry(0,0,700,1200);
-    QVBoxLayout *layout=new QVBoxLayout(ui->scrollAreaWidgetContents);
-//    QLabel *label=new QLabel();
-//    label->setText("111");
-//    QLabel *label2=new QLabel();
-//    label2->setText("222");
-//    layout->addWidget(label);
-//    layout->addWidget(label2);
-    QTextEdit *textEdit=new QTextEdit;
-    layout->addWidget(textEdit);
-    connect(ui->ok,&QPushButton::clicked,this,&LongShot::longShot);
-     //
-//      QString txt=textEdit->toPlainText();
-//      qDebug()<<txt;
-//    textEdit->textCursor().insertImage(QImage("/root/MyProject/2.jpg"));
-   //
+    ui->scrollAreaWidgetContents->setGeometry(0,0,600,700);
+    layout=new QVBoxLayout(ui->scrollAreaWidgetContents);
+    ui->scrollAreaWidgetContents->setMaximumHeight(500);
 
+    connect(ui->openImage,&QPushButton::clicked,this,&LongShot::open);
+    connect(ui->ok,&QPushButton::clicked,this,&LongShot::longShot);
+    textEdit=new QTextEdit;
+    label=new QLabel;
 }
 
+//保存截图图片
 void LongShot::longShot()
 {
     QPixmap pix(ui->scrollAreaWidgetContents->size());
     ui->scrollAreaWidgetContents->render(&pix);
-    pix.save("/root/MyProject/1.jpg");
+    QString path=QFileDialog::getSaveFileName(this,tr("另存为"),tr(""),tr("图片(*.png *.jpeg *.jpg)"));
+    pix.save(path);
+}
+//打开文件，添加要加入的图片
+void LongShot::open()
+{
+    path=QFileDialog::getOpenFileName();
+    m_Image.load(path);
+    m_pixmap = QPixmap::fromImage(m_Image).scaled(size()/*,Qt::KeepAspectRatio,Qt::SmoothTransformation*/);
+    label->setPixmap(m_pixmap);
+    label->setScaledContents(true);
+    layout->addWidget(label);
+    layout->addWidget(textEdit);
+    update();
 }
 
 LongShot::~LongShot()
 {
     delete ui;
+    delete label;
+    delete textEdit;
+    delete layout;
+    label=nullptr;
+    textEdit=nullptr;
+    layout=nullptr;
 }
--- ScreenShot/longshot.h
+++ ScreenShot/longshot.h
@@ -1,7 +1,6 @@
-/*author:longxinping 2020051615040
- 存在的问题：暂时只能在滚动条所在的窗口输入文本，进行截取长图。
- 输入文本后按OK按钮进行截取图片。
- 后续继续改进*/
+//author:longxinping 2020051615040
+//This is a program to set LongShot
+//这相当于一个写日记，记录生活的长截图功能，把拍的图片放入窗口中，在图片下面的文本框中输入当时的感受，想表达的心情，然后按截图按钮截长图
 
 #ifndef LONGSHOT_H
 #define LONGSHOT_H
@@ -9,6 +8,8 @@
 #include<QPushButton>
 #include <QWidget>
 #include<QScrollArea>
+#include<QTextEdit>
+#include<QLabel>
 #include"ui_form.h"
 
 class LongShot:public QWidget
@@ -18,9 +19,15 @@ public:
     LongShot(QWidget *parent=0);
     ~LongShot();
 public slots:
+    void open();
     void longShot();
 private:
     Ui::Form *ui;
-
+    QImage m_Image;
+    QPixmap m_pixmap;
+    QLabel *label;
+    QVBoxLayout *layout;
+    QString path;
+    QTextEdit *textEdit;
 };
 #endif // LONGSHOT_H
